6e72523cdd70867afdce06c6cf490b7042e659bb
bigbiff <bigbiff@teamw.in>
2016-02-19 21:08:45 -0500

    We don't need GPS makefile.

--- device.mk
+++ device.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
 $(call inherit-product-if-exists, vendor/emulator/twrp/twrp-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/emulator/twrp/overlay
